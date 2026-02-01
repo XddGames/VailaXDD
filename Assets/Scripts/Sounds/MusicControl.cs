0308b65d5e00c87c8d65bfbe5919f4e29c6d73c3
Gonki04 <goncaloaraujo2004@gmail.com>
2026-02-01 16:28:58 +0000

    adad

--- Assets/Scripts/Sounds/MusicControl.cs
+++ Assets/Scripts/Sounds/MusicControl.cs
@@ -4,16 +4,64 @@ public class MusicControl : MonoBehaviour
 {
     private static MusicControl instance;
 
+    [Header("Background Music")]
+    [SerializeField] private AudioClip musicClip;
+    [SerializeField] [Range(0f, 1f)] private float musicVolume = 0.3f;
+    
+    private AudioSource audioSource;
+
     void Awake()
     {
         if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
+            
+            // Setup AudioSource for background music
+            audioSource = GetComponent<AudioSource>();
+            if (audioSource == null)
+            {
+                audioSource = gameObject.AddComponent<AudioSource>();
+            }
+            
+            audioSource.loop = true;
+            audioSource.playOnAwake = false;
+            audioSource.volume = musicVolume;
         }
         else
         {
             Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        PlayMusic();
+    }
+
+    public void PlayMusic()
+    {
+        if (audioSource != null && musicClip != null)
+        {
+            audioSource.clip = musicClip;
+            audioSource.Play();
+        }
+    }
+
+    public void StopMusic()
+    {
+        if (audioSource != null)
+        {
+            audioSource.Stop();
+        }
+    }
+
+    public void SetVolume(float volume)
+    {
+        musicVolume = Mathf.Clamp01(volume);
+        if (audioSource != null)
+        {
+            audioSource.volume = musicVolume;
         }
     }
 }
